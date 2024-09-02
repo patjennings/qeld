6ee2487cc64fbd83933e1548c4329e7b39d39ccb
Thomas Guesnon <>
2024-09-02 23:49:54 +0200

    new season

--- season_manager/apps.py
+++ season_manager/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 class SeasonManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'season_manager'
-    SEASON_MANAGER_SEASON = '2023-2024'
+    SEASON_MANAGER_SEASON = '2024-2025'
     SEASON_MANAGER_HEADER = 'Piliers de Kernilis'
